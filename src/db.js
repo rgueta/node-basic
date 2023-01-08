cf9a418dbecfbc68fc4e6e2fc1be505f8b6e083d
Gueta <ricardogueta@hotmail.com>
2023-01-07 22:47:36 -0800

    DB_URL removed from app.js

--- src/db.js
+++ src/db.js
@@ -15,9 +15,8 @@
 //  ---------- MONGO  ---------------------------
 
 import mongoose from "mongoose";
-import { DB_HEAD_URL, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URL } from "./config.js";
+import { DB_HEAD_URL, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } from "./config.js";
 
-console.log('url -> ', DB_URL)
 
 mongoose.set('strictQuery',false)
 mongoose.connect(`${DB_HEAD_URL}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`).then(() => console.log('DB Connected!'));
